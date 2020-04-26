52fc6fa18e17a2d1fadd9079ce9b6d9984546fb5
Ceruin <13532471+Ceruin@users.noreply.github.com>
2020-04-26 16:50:43 -0400

    Overlay Text Mesh

--- Realm-Defenders/Assets/EditorSnap.cs
+++ Realm-Defenders/Assets/EditorSnap.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [ExecuteInEditMode]
+[SelectionBase]
 public class EditorSnap : MonoBehaviour
 {
-    [SerializeField][Range(1f, 20f)] float gridSize = 10f; 
+    [SerializeField][Range(1f, 20f)] float gridSize = 10f;
+
+    TextMesh textMesh;
 
     // Update is called once per frame
     void Update()
     {
         Vector3 snapPos;
+
         snapPos.x = GetSnapPos(transform.position.x);
         snapPos.z = GetSnapPos(transform.position.z);
-
         transform.position = new Vector3(snapPos.x, 0f, snapPos.z);
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPos.x  / gridSize + "," + snapPos.z / gridSize;
     }
 
     private float GetSnapPos(float transformPos)
