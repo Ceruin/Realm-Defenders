78935cf0c01c9c3afc8b4cf2162924858376274a
Ceruin <13532471+Ceruin@users.noreply.github.com>
2020-05-15 00:53:50 -0400

    Enemy Spawner

---
+++ Realm-Defenders/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] float secondsBetweenSpawns;
+    [SerializeField] EnemyMovement Enemy;
+    int totalEnemies = 100;
+
+    // Update is called once per frame
+    void Start()
+    {
+        StartCoroutine(RepeatedlySpawnEnemies());
+    }
+
+    IEnumerator RepeatedlySpawnEnemies()
+    {
+        while (totalEnemies > 0) // true for forever
+        {
+            print("Spawning");
+            Instantiate(Enemy);
+            totalEnemies--;
+            yield return new WaitForSeconds(secondsBetweenSpawns);
+        }
+    }
+}
